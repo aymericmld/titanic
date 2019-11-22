bd9ac841c5f14380c50b15699239bce5a9e13db2
aymericmld <aymeric.maillard@gmail.com>
2019-11-22 12:22:27 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -8,3 +8,16 @@ print(df_test)
 df_train = pd.read_csv("BDD/train.csv", encoding = "utf-8")
 df_train = df_train.set_index('PassengerId')
 print(df_train)
+
+
+df_train['Age'].fillna(df_train['Age'].median(), inplace=True)
+df_train.describe()
+
+df_train[df_train['Survived']==1]['Sex'].value_counts()
+df_train[df_train['Survived']==0]['Sex'].value_counts()
+
+survived_sex = df_train[df_train['Survived']==1]['Sex'].value_counts()
+dead_sex = df_train[df_train['Survived']==0]['Sex'].value_counts()
+df_s = pd.DataFrame([survived_sex,dead_sex])
+df_s.index = ['Survived','Dead']
+df_s.plot(kind='bar',stacked=True, figsize=(15,8))
